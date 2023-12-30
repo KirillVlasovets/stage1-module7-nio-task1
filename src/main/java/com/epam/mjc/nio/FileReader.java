6fa2361496e081c893b8bd76dd35b073041b34b0
Kirill Vlasovets <k.vlasovets@a1qa.com>
2023-12-31 01:09:03 +0400

    solution added

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,39 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.util.logging.Logger;
 
 
 public class FileReader {
 
+    private static final Logger logger = Logger.getLogger(FileReader.class.getName());
+
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (InputStream inputStream = new FileInputStream(file)) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                setProfileField(profile, line);
+            }
+        } catch (IOException e) {
+            logger.info(e.getMessage());
+        }
+        return profile;
+    }
+
+    private void setProfileField(Profile profile, String line) {
+        String value = line.split("\\s")[1];
+        if (line.contains("Name")) {
+            profile.setName(value);
+        } else if (line.contains("Age")) {
+            profile.setAge(Integer.parseInt(value));
+        } else if (line.contains("Email")) {
+            profile.setEmail(value);
+        } else if (line.contains("Phone")) {
+            profile.setPhone(Long.parseLong(value));
+        } else {
+            logger.info("Not existed field");
+        }
     }
 }
